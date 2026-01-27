1a418542228c6367b20e6f29b429cad82a15d701
Elene Chanturia <elenechanturia104@gmail.com>
2026-01-27 13:23:24 +0000

    mainc ar aris naTargmni

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="ka" suppressHydrationWarning>
       <head>
         <link rel="stylesheet" href="https://fonts.ge/css/bpg-nino-mtavruli.css" />
       </head>
