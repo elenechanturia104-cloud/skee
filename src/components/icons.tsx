f39f342442d1b4cea8b0dd15a571d10c032eaece
Elene Chanturia <elenechanturia104@gmail.com>
2026-01-28 05:39:38 +0000

    არ ჩანს სურათი

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -4,7 +4,7 @@ export const ChronoBoardLogo = ({ className }: { className?: string }) => {
   return (
     <div className={`relative ${className}`}>
       <Image
-        src="https://i.ibb.co/68R0B1c/chronoboard-logo.png"
+        src="https://i.ibb.co/yYyMh1Q/chronoboard-logo.png"
         alt="ChronoBoard Logo"
         fill
         className="object-contain"
